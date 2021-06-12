d2e80797ed0db0347bdd001a8b00e9c583482241
“qq4215279” <liu865488158@qq.com>
2021-06-12 18:06:56 +0800

    Final 关键字 使用

--- TestDay/src/test/java/com/gobestsoft/javaBase/keyword/FinalDemo.java
+++ TestDay/src/test/java/com/gobestsoft/javaBase/keyword/FinalDemo.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 /**
  * FinalDemo
- * Final 关键字修饰常量时的使用
+ * final 关键字修饰常量时的使用
  * @author liuzhen
  * @version 1.0.0 2021/5/23 20:29
  */
@@ -27,17 +27,45 @@ public class FinalDemo {
      * 对于一个final变量，如果是基本数据类型的变量，则其数值一旦在初始化之后便不能更改；如果是引用类型的变量，则在对其初始化之后便不能再让其指向另一个对象。
      */
     public void useFinal() {
-//        num = 220;
-//        numInteger = 200;
-//        str = "bb";
+//        num = 220;  // 报错,不可重新赋值
+//        numInteger = 200;  // 报错,不可重新赋值
+//        str = "bb";  // 报错,不可重新赋值
         final Object obj = new Object();
-//        obj = new Object();
+//        obj = new Object();  // 报错,不可重新赋值
 //
         map.put("a", 2);
     }
 
-    public static void main(String[] args) {
+    public class User {
+        public String name;
+        public int age;
 
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
+        }
+    }
+
+    /**
+     * final修饰引用类型时，在对其初始化之后便不能再让其指向另一个对象。但被引用的对象的值是可以改变的（引用变量的值实际上是它所引用的对象的地址）。
+     */
+    public void referenceObjDemo() {
+        final User user = new User("haha", 5);
+        user.name = "李华";
+        user.age = 10;
+        System.out.println(user); // 证明了：它指向的对象的内容是可变的。
+
+//        user = new User(); //  // 报错,不可重新赋值
+    }
+
+    public static void main(String[] args) {
+        FinalDemo demo = new FinalDemo();
+        demo.referenceObjDemo();
     }
 
 }
