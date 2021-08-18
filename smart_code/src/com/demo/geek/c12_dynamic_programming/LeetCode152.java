0b6ab290fe47cdac6b080fab1803d954c4e267e1
“qq4215279” <liu865488158@qq.com>
2021-08-18 23:00:52 +0800

    add DP

---
+++ smart_code/src/com/demo/geek/c12_dynamic_programming/LeetCode152.java
@@ -0,0 +1,30 @@
+package com.demo.geek.c12_dynamic_programming;
+
+/**
+ * LeetCode152
+ * 乘积最大子数组
+ * @author liuzhen
+ * @version 1.0.0 2021/8/18 21:02
+ */
+public class LeetCode152 {
+
+    /**
+     * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+     *
+     * 示例 1:
+     *
+     * 输入: [2,3,-2,4]
+     * 输出: 6
+     * 解释: 子数组 [2,3] 有最大乘积 6。
+     *
+     * 示例 2:
+     * 输入: [-2,0,-1]
+     * 输出: 0
+     * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+     */
+
+    public int maxProduct(int[] nums) {
+
+        return 0;
+    }
+}
