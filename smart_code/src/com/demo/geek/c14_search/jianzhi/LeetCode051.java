6d481b261eed040e176d2222e0bea23e30e6bf0e
“qq4215279” <liu865488158@qq.com>
2021-08-24 22:24:16 +0800

    add 高级搜索

---
+++ smart_code/src/com/demo/geek/c14_search/jianzhi/LeetCode051.java
@@ -0,0 +1,10 @@
+package com.demo.geek.c14_search.jianzhi;
+
+/**
+ * LeetCode051
+ * N皇后
+ * @author liuzhen
+ * @version 1.0.0 2021/8/24 8:47
+ */
+public class LeetCode051 { // c8_divide_backtrack
+}
