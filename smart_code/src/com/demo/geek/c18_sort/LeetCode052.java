bc3e8b8afe2c4ef690a4177f05d297e071797a2a
“qq4215279” <liu865488158@qq.com>
2021-08-29 23:45:52 +0800

    add 排序

---
+++ smart_code/src/com/demo/geek/c18_sort/LeetCode052.java
@@ -0,0 +1,35 @@
+package com.demo.geek.c18_sort;
+
+/**
+ * LeetCode052
+ * 合并区间
+ * @author liuzhen
+ * @version 1.0.0 2021/8/28 23:20
+ */
+public class LeetCode052 {
+
+    /**
+     * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+     *
+     * 示例 1：
+     * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * 输出：[[1,6],[8,10],[15,18]]
+     * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     *
+     * 示例 2：
+     * 输入：intervals = [[1,4],[4,5]]
+     * 输出：[[1,5]]
+     * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+     *
+     * 提示：
+     * 1 <= intervals.length <= 104
+     * intervals[i].length == 2
+     * 0 <= starti <= endi <= 104
+     */
+
+    public int[][] merge(int[][] intervals) {
+
+        return null;
+    }
+
+}
---
+++ smart_code/src/com/demo/geek/c18_sort/LeetCode493.java
@@ -0,0 +1,129 @@
+package com.demo.geek.c18_sort;
+
+import java.util.Arrays;
+
+/**
+ * LeetCode493
+ * 翻转对
+ * @author liuzhen
+ * @version 1.0.0 2021/8/28 23:22
+ */
+public class LeetCode493 {
+
+    /**
+     * 给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+     * 你需要返回给定数组中的重要翻转对的数量。
+     *
+     * 示例 1:
+     * 输入: [1,3,2,3,1]
+     * 输出: 2
+     *
+     * 示例 2:
+     * 输入: [2,4,3,5,1]
+     * 输出: 3
+     *
+     * 注意:
+     * 给定数组的长度不会超过50000。
+     * 输入数组中的所有数字都在32位整数的表示范围内。
+     *
+     * 思路：
+     * 1. 暴力法：两个嵌套循环：O(n^2)
+     * 2. merge-sort
+     * 3. 树状数组
+     *
+     */
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] nums, int s, int e) {
+        if (s >= e) {
+            return 0;
+        }
+
+        int mid = s + (e - s) / 2;
+        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid + 1, e);
+        for (int i = s, j = mid + 1; i <= mid; i++) {
+            while (j <= e && nums[i] / 2.0 > nums[j]) {
+                j++;
+            }
+            cnt += j - (mid + 1);
+        }
+
+        Arrays.sort(nums, s, e + 1);
+        return cnt;
+    }
+
+    public int ret;
+    public int reversePairs2(int[] nums) {
+        ret = 0;
+        mergeSort2(nums, 0, nums.length - 1);
+        
+        return ret;
+    }
+
+    private void mergeSort2(int[] nums, int left, int right) {
+        if (right <= left) {
+            return;
+        }
+        int middle = left + (right - left) / 2;
+        mergeSort2(nums, left, middle);
+        mergeSort2(nums, middle + 1, right);
+        
+        // count elements
+        int count = 0;
+        for (int l = left, r = middle + 1; l <= middle;) {
+            if (r > right || (long)nums[l] <= 2 * (long)nums[r]) {
+                l++;
+                ret += count;
+            } else {
+                r++;
+                count++;
+            }
+        }
+
+        // sort
+        Arrays.sort(nums, left, right + 1);
+    }
+
+
+    public int reversePairs3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        return mergeSort3(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort3(int[] nums, int l, int r) {
+        if (l >= r) {
+            return 0;
+        }
+
+        int mid = l + (r - l) / 2;
+        int count = mergeSort3(nums, l, mid) + mergeSort3(nums, mid + 1, r);
+        int[] cache = new int[r - l + 1];
+        int i = l;
+        int t = l;
+        int c = 0;
+
+        for (int j = mid + 1; j <= r; j++, c++) {
+           while (i <= mid && nums[i] <= 2 * (long)nums[j]) {
+               i++;
+           }
+           while (t <= mid && nums[t] < nums[j]) {
+               cache[c++] = nums[t++];
+           }
+            cache[c] = nums[j];
+            count += mid - i + 1;
+        }
+
+        while (t <= mid) {
+            cache[c++] = nums[t++];
+        }
+        System.arraycopy(cache, 0, nums, l, r - l + 1);
+        return count;
+    }
+
+}
