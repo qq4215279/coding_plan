540757b6e0767841af51da784c954eaa75df248a
liuzhen <liu865488158@qq.com>
2021-12-20 23:35:33 +0800

    add 接口与抽象类

--- tech_enhance/src/test/java/com/gobestsoft/java_base/func_interface/JDK8InterFaceTest.java
+++ tech_enhance/src/test/java/com/gobestsoft/java_base/func_interface/JDK8InterFaceTest.java
@@ -35,6 +35,9 @@ public interface JDK8InterFaceTest {
      * 因为改动接口所有的实现类都要改动,所以增加了default关键字后不需要修改其他类,默认给所有实现类增加了方法.
      * 接口中可定义方法（public类型的）
      * 默认interface中指定的方法都要override实现的，但是default修饰的可以不用重写了，直接可以使用。
+     *
+     * default是Java1.8的新特性，接口内不可以实现具体的函数，前提是使用default修饰，因此在实现使用了default修饰方法的接口的时候，可以重写接口default，可以默认使用父类方法，具有多态性。
+     *
      * @return
      */
     default int defaultMethodB() {
