2ad40759f5797e767f510f0bd29d5c2f48fdb06c
liuzhen <liu865488158@qq.com>
2022-07-17 17:22:46 +0800

    add study reflect

--- tech_enhance/src/test/java/com/gobestsoft/java_base/reflect/reflect/ReflectDemo2.java
+++ tech_enhance/src/test/java/com/gobestsoft/java_base/reflect/reflect/ReflectDemo2.java
@@ -16,18 +16,30 @@ public class ReflectDemo2 {
      * 1. 获取成员变量们
      *  Field[] getFields() Field getField(String name)
      *  Field[] getDeclaredFields() Field getDeclaredField(String name)
-     *
      * 2. 获取构造方法们
      *  Constructor<?>[] getConstructors()
      *  Constructor<T> getConstructor(类<?>... parameterTypes)
-     *  Constructor<T> getDeclaredConstructor(类<?>... parameterTypes) Constructor<?>[] getDeclaredConstructors()
-     *
+     *  Constructor<T> getDeclaredConstructor(类<?>... parameterTypes)
+     *  Constructor<?>[] getDeclaredConstructors()
      * 3. 获取成员方法们：
      *  Method[] getMethods() Method getMethod(String name, 类<?>... parameterTypes)
      *  Method[] getDeclaredMethods() Method getDeclaredMethod(String name, 类<?>... parameterTypes)
-     * 
      * 4. 获取类名 String getName()
-     * 
+     *
+     * Field：成员变量
+     * 操作：
+     * 1. 设置值：void set(Object obj, Object value)
+     * 2. 获取值：get(Object obj)
+     * 3. 忽略访问权限修饰符的安全检查：setAccessible(true):暴力反射
+     *
+     * Constructor:构造方法
+     * 创建对象：T newInstance(Object... initargs)
+     * 如果使用空参数构造方法创建对象，操作可以简化：Class对象的newInstance方法
+     *
+     * Method：方法对象
+     * 执行方法：Object invoke(Object obj, Object... args)
+     * 获取方法名称：String getName:获取方法名
+     *
      */
 
     public static void main(String[] args) throws Exception {
