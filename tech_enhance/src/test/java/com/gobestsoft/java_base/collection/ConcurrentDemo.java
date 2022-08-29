7ffd6ac2ff020c6305479111d7de23a082e9c90f
Usernameliuzhen <liuzhen@hariogame.com>
2022-08-29 18:46:05 +0800

    add study 并发

---
+++ tech_enhance/src/test/java/com/gobestsoft/java_base/collection/ConcurrentDemo.java
@@ -0,0 +1,64 @@
+package com.gobestsoft.java_base.collection;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * ConcurrentDemo
+ *
+ * @author liuzhen
+ * @version 1.0.0 2022/8/29 14:35
+ */
+public class ConcurrentDemo {
+
+    private static List<Team> teamList = new CopyOnWriteArrayList<>();
+
+    private static Object object = new Object();
+
+    public static List<Team> getTeamList() {
+        synchronized (object) {
+            return teamList;
+        }
+    }
+
+    public static void main(String[] args) {
+        teamList.add(new Team(1, "zhangsan", 100));
+        teamList.add(new Team(2, "刘亦菲", 99));
+        teamList.add(new Team(3, "姚明", 100));
+
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                for (Team team : getTeamList()) {
+                    System.out.println(team.toString());
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (object) {
+                teamList.add(new Team(4, "4", 4));
+            }
+        }).start();
+    }
+
+    static class Team {
+        public int playerId;
+        public String playerName;
+        public int lv;
+
+        public Team() {
+        }
+
+        public Team(int playerId, String playerName, int lv) {
+            this.playerId = playerId;
+            this.playerName = playerName;
+            this.lv = lv;
+        }
+
+        @Override
+        public String toString() {
+            return "Team{" + "playerId=" + playerId + ", playerName='" + playerName + '\'' + ", lv=" + lv + '}';
+        }
+    }
+
+}
