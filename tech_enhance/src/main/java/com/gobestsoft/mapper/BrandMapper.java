ed0302a6f388e1e33d2d71043a513730085cf135
qq4215279 <liu865488158@qq.com>
2023-06-02 16:41:39 +0800

    delete no use class

--- tech_enhance/src/main/java/com/gobestsoft/mapper/BrandMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gobestsoft.mapper;
-
-import com.gobestsoft.pojo.Brand;
-import tk.mybatis.mapper.common.Mapper;
-
-
-@org.apache.ibatis.annotations.Mapper
-public interface BrandMapper extends Mapper<Brand> {
-}
