d55250b90b3aefa2121b8aa70b396000ebb90da2
qq4215279 <liu865488158@qq.com>
2023-07-22 18:03:54 +0800

    异常 整理

---
+++ tech_enhance/src/main/java/com/mumu/java_base/collection/IntArray2List.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020-2023, 木木996.
+ * All Right Reserved.
+ */
+
+package com.mumu.java_base.collection;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * IntArray2List
+ * int数组转list
+ * @author liuzhen
+ * @version 1.0.0 2023/7/22 16:42
+ */
+public class IntArray2List {
+
+    /**
+     * int[]转list
+     * @date 2023/7/22 16:43
+     * @param
+     * @return void
+     */
+    @Test
+    public void intArray2ListTest(){
+        int[] array = {1, 2, 5, 5, 5, 5, 6, 6, 7, 2, 9, 2};
+
+        // 方法一：需要导入apache commons-lang3 jar
+        List<Integer> list = Arrays.asList(ArrayUtils.toObject(array));
+        System.out.println(list.toString());
+
+        // 方法二：java8及以上版本
+        List<Integer> list1 = Arrays.stream(array).boxed().collect(Collectors.toList());
+        System.out.println(list1.toString());
+
+        // 方法三：for循环
+        List<Integer> list3 = new ArrayList<>();
+        for (int i : array) {
+            list3.add(i);
+        }
+    }
+
+    @Test
+    public void intArray2IntegertArrayTest(){
+        int[] array = {1, 2, 5, 5, 5, 5, 6, 6, 7, 2, 9, 2};
+
+        // int[] 转 Integer[]
+        Integer[] integerArr = Arrays.stream(array).boxed().toArray(Integer[]::new);
+        System.out.println(Arrays.toString(integerArr));
+    }
+
+    @Test
+    public void list2IntArrayTest(){
+        List<Integer> list = List.of(1, 2, 3);
+
+        // 方法二：java8及以上版本(需要转为int[]的请用这种)
+        int[] intArr1 =  list.stream().mapToInt(Integer::valueOf).toArray();
+        System.out.println(Arrays.toString(intArr1));
+    }
+
+    @Test
+    public void list2IntegerArrayTest(){
+        List<Integer> list = List.of(1, 2, 3);
+
+        Integer[] integerArr = list.toArray(new Integer[0]);
+        // Integer[] integerArr =  list.toArray(new Integer[list.size()]);
+    }
+
+}
