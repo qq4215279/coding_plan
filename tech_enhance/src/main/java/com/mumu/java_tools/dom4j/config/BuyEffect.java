e4c8a9ca350b4089c2f8d2ead6ead18acbe61c2f
qq4215279 <liu865488158@qq.com>
2024-05-17 14:44:14 +0800

    dom4j study

---
+++ tech_enhance/src/main/java/com/mumu/java_tools/dom4j/config/BuyEffect.java
@@ -0,0 +1,117 @@
+
+package com.mumu.java_tools.dom4j.config;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.math.BigInteger;
+
+
+/**
+ * <p>anonymous complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence minOccurs="0">
+ *         &lt;element ref="{}id"/>
+ *         &lt;element ref="{}cost"/>
+ *         &lt;element ref="{}num"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "id",
+    "cost",
+    "num"
+})
+@XmlRootElement(name = "buyEffect")
+public class BuyEffect {
+
+    protected BigInteger id;
+    protected BigInteger cost;
+    protected BigInteger num;
+
+    /**
+     * 获取id属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getId() {
+        return id;
+    }
+
+    /**
+     * 设置id属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setId(BigInteger value) {
+        this.id = value;
+    }
+
+    /**
+     * 获取cost属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getCost() {
+        return cost;
+    }
+
+    /**
+     * 设置cost属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setCost(BigInteger value) {
+        this.cost = value;
+    }
+
+    /**
+     * 获取num属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getNum() {
+        return num;
+    }
+
+    /**
+     * 设置num属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setNum(BigInteger value) {
+        this.num = value;
+    }
+
+}
