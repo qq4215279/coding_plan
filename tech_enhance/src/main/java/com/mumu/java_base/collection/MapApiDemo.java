5cc3928527d3d4c3a78353c2771fa7646862e21d
qq4215279 <liu865488158@qq.com>
2024-07-05 14:39:36 +0800

    going map

--- tech_enhance/src/main/java/com/mumu/java_base/collection/MapApiDemo.java
+++ tech_enhance/src/main/java/com/mumu/java_base/collection/MapApiDemo.java
@@ -13,12 +13,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * PutApis
+ * MapApiDemo
  *
  * @author liuzhen
  * @version 1.0.0 2021/7/30 16:34
  */
-public class PutApis {
+public class MapApiDemo {
 
 
     /**
@@ -57,6 +57,10 @@ public class PutApis {
     /**
      * compute：V compute(K key, BiFunction < ? super K, ? super V, ? extends V> remappingFunction)
      * compute的方法,指定的key在map中的值进行操作 不管存不存在，操作完成后保存到map中
+     * 注：
+     *  - 使用 compute() 需要进行对 V 值进行判空，若V为null会报空指针异常！eg: integer4
+     *  - compute() 返回为最新的 V 值
+     *
      * @return void
      * @date 2024/5/20 16:04
      */
@@ -67,15 +71,18 @@ public class PutApis {
         map.put("2", 2);
         map.put("3", 3);
         Integer integer = map.compute("3", (k, v) -> v + 1);
-        //key不管存在不在都会执行后面的函数，并保存到map中
-        Integer integer1 = map.compute("4", (k, v) -> {
+        // v 为null，需要对v 进行判空，如下写法error
+        // Integer integer4 = map.compute("4", (k, v) -> v + 1);
+        // key不管存在不在都会执行后面的函数，并保存到map中
+        Integer integer5 = map.compute("5", (k, v) -> {
             if (v == null) {
                 return 0;
             }
             return v + 1;
         });
+
         System.out.println(integer); // 4
-        System.out.println(integer1); // 0
+        System.out.println(integer5); // 0
         System.out.println(map.toString()); // {1=1, 2=2, 3=4, 4=0}
     }
 
