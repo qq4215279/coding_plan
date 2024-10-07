fc664797d59e71a424ae10d018e57e550937be76
qq4215279 <liu865488158@qq.com>
2024-10-07 20:11:10 +0800

    delete no use package

--- tech_enhance/src/main/java/com/mumu/java_tools/dom4j/config/ConfigItemInfo.java
+++ tech_enhance/src/main/java/com/mumu/java_tools/dom4j/config/ConfigItemInfo.java
@@ -1,14 +1,6 @@
 
 package com.mumu.java_tools.dom4j.config;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
